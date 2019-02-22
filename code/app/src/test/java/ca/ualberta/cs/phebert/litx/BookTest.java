87c95c04ca5584a09fac8a00fec2da0c79f8d4ed
phebert5009 <cpierrot273@gmail.com>
2019-02-22 11:15:30 -0700

    ISBN should be a long as it is 13 digits.

--- code/app/src/test/java/ca/ualberta/cs/phebert/litx/BookTest.java
+++ code/app/src/test/java/ca/ualberta/cs/phebert/litx/BookTest.java
@@ -6,11 +6,40 @@ import static org.junit.Assert.*;
 
 public class BookTest {
     @Test
-    public void Owner_isCorrect()
+    public void owner_isCorrect()
     {
         Owner owner = new Owner();
         Book book = new Book();
+        assertNull(book.getOwner());
         book.setOwner(owner);
         assertEquals(owner, book.getOwner());
+    }
+
+    @Test
+    public void available_isCorrect()
+    {
+        Book book = new Book();
+        assertTrue(book.getAvailable())
+        book.setAvailable(true);
+        assertTrue(book.getAvailable());
+        book.setAvailable(false);
+        assertFalse(book.getAvailable());
+        book.setAvailable(false);
+        assertFalse(book.getAvailable());
+        book.setAvailable(true);
+        assertTrue(book.getAvailable());
+    }
+
+    @Test
+    public void available_isIndependant()
+    {
+        Book book = new Book();
+        Owner originalOwner = book.getOwner();
+        String originalAuthor = book.getAuthor();
+        long originalISBN = book.getISBN();
+        book.setAvailable(false);
+        assertEquals(originalOwner, book.getOwner());
+        assertEquals(originalAuthor, book.getAuthor());
+        assertEquals(originalISBN, book.getISBN());
     }
 }
