906d15d65624f8587a3dc07833b994251ac17633
phebert5009 <cpierrot273@gmail.com>
2019-02-24 16:11:53 -0700

    added Intents.

--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/MainActivity.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/MainActivity.java
@@ -1,13 +1,34 @@
 package ca.ualberta.cs.phebert.litx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String FilterMode = "ca.ualberta.cs.phebert.litx.FilterMode";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void filter(View v) {
+        Intent intent = new Intent(this, FilteredActivity.class);
+        if(v.getId() == R.id.requests_home) {
+            intent.putExtra(FilterMode,0);
+        } else if(v.getId() == R.id.accept_home) {
+            intent.putExtra(FilterMode,1);
+        } else { // borrowed
+            intent.putExtra(FilterMode,2);
+        }
+        startActivity(intent);
+    }
+
+    public void goToProfileView(View v) {
+        Intent intent = new Intent(this,ProfileActivity.class);
+        startActivity(intent);
     }
 }
