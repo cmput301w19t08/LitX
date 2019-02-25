a4d7803f7b067f458004ca8ceb8da6102ea10019
phebert5009 <cpierrot273@gmail.com>
2019-02-24 21:12:32 -0700

    Simplified search.

--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/Search.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/Search.java
@@ -1,23 +1,7 @@
 package ca.ualberta.cs.phebert.litx;
 
-public class Search {
-    private User borrower;
-    private ArrayList<Book> results;
-    private int numResults; //number of results we get, may not be necessary
-    private String authorKeyWord;
-    private String titleKeyWord;
-    private int ISBNKeyWord;
+import java.util.ArrayList;
 
-    /*
-     * This should be called by the constructor. Checks either a database with all books
-     * or all the books of all the owners.
-     */
-    public ArrayList<Book> findBooks() {
-        return books;
-    }
-
-    // Runs a layout that displays all the books in results using their getter methods
-    public void displayResults() {
-
-    }
+public interface Search {
+    ArrayList<Book> find(ArrayList<Book> Original, String[] keywords);
 }
