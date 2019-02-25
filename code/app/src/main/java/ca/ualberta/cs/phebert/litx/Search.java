39a79884e34dd28b0487977cd45b584f947d737c
phebert5009 <cpierrot273@gmail.com>
2019-02-24 21:17:42 -0700

    added a comment.

--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/Search.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/Search.java
@@ -2,6 +2,7 @@ package ca.ualberta.cs.phebert.litx;
 
 import java.util.ArrayList;
 
+// could be a class with a proper implementation
 public interface Search {
     ArrayList<Book> find(ArrayList<Book> Original, String[] keywords);
 }
