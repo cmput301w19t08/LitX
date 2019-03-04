5274afe774f5a798af97d04ac214f60726424e0c
phebert5009 <cpierrot273@gmail.com>
2019-03-04 13:50:46 -0700

    start by heading towards profileView if user is not authenticated.

--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/MainActivity.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
     public static final String FilterMode = "ca.ualberta.cs.phebert.litx.FilterMode";
 
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            goToProfileView(null);
+        }
     }
 
     public void filter(View v) {
