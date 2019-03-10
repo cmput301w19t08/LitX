aa31252a0f54972670d16562c4755daba69aece5
Cohen Plontke <plontke@ualberta.ca>
2019-03-09 18:44:24 -0700

    Added the OnClick Listener to the BookList adapter

--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/BookViewActivity.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/BookViewActivity.java
@@ -1,10 +1,13 @@
 package ca.ualberta.cs.phebert.litx;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * superclass for myBooks and for MyBooksActivity and searchActivity.
@@ -18,6 +21,14 @@ public class BookViewActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_book);
+        Intent intent = getIntent();
+        Book book = (Book) intent.getExtras().getSerializable("Book");
+
+        ImageView image = (ImageView) findViewById(R.id.bookImage);
+        TextView textView = (TextView) findViewById(R.id.descriptionIDView);
+        textView.setText(book.getTitle());
+        image = book.getPhotograph();
+
 
         delete = (Button) findViewById(R.id.deleteButtonID);
         edit = (Button) findViewById(R.id.editButtonID);
