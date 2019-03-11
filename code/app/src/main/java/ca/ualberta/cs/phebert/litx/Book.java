900cb993203d05536e4e9ee572720b774fdea56d
Cohen Plontke <plontke@ualberta.ca>
2019-03-11 11:55:20 -0600

    Added status to Book and javadocced Book, added filtering to MyBooksActivity

--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/Book.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/Book.java
@@ -9,6 +9,15 @@ public class Book implements Serializable {
     private String author;
     private String title;
     private long isbn;
+    private Status status;
+
+
+    enum Status {
+        Available,
+        Borrowed,
+        Accepted,
+        Requested
+    }
     // For testing the adapter
     //private User borrower;
 
@@ -26,49 +35,134 @@ public class Book implements Serializable {
         this.isbn = isbn;
     }
 
-    public Book() {} // For firestore
+    public Book() {
+    } // For firestore
 
-    public void setOwner(String owner) { this.owner = owner; }
-
-    public String getOwner() { return owner; }
-
-    public Boolean isAvailable() {
-        return acceptedRequest == null;
+    /**
+     * sets Owner of the book
+     *
+     * @param owner string of the owner of the book
+     */
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 
+    /**
+     * getter for status
+     *
+     * @return String
+     */
+    public String getStatus() {
+        return this.status.toString();
+    }
+
+    /**
+     * Sets this.status to status
+     *
+     * @param status String one of accepted, available, borrowed, requested
+     */
+    public void setStatus(String status) {
+        if (status.equals("accepted")) {
+            this.status = Status.Accepted;
+        } else if (status.equals("available")) {
+            this.status = Status.Available;
+        } else if (status.equals("borrowed")) {
+            this.status = Status.Borrowed;
+        } else {
+            this.status = Status.Requested;
+        }
+
+    }
+
+    /**
+     * Getter for owner
+     *
+     * @return String
+     */
+    public String getOwner() {
+        return owner;
+    }
+
+    /**
+     * Returns if the book is available
+     *
+     * @return Boolean
+     */
+    public Boolean isAvailable() {
+        return this.status == Status.Available;
+    }
+
+    /**
+     * getter for author
+     *
+     * @return String
+     */
     public String getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) { this.author = author; }
+    /**
+     * setter for Author
+     *
+     * @param author String
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
+    /**
+     * getter for title
+     *
+     * @return String
+     */
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) { this.title = title; }
+    /**
+     * Setter for Title
+     *
+     * @param title String
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    /**
+     * Getter for ISBN
+     *
+     * @return long type
+     */
     public long getIsbn() {
         return isbn;
     }
 
+    /**
+     * setter for ISBN
+     *
+     * @param isbn long type
+     */
     public void setIsbn(long isbn) {
         this.isbn = isbn;
     }
 
-
+    /**
+     * getter for requests
+     *
+     * @returns ArrayList<Request>
+     */
     public ArrayList<Request> getRequests() {
         return requests;
     }
 
 
-    /*public void setBorrower(User user) {
-        this.borrower = user;
-
-    }*/
-
+    /**
+     * Getter for borrower, if no acceptedRequest then returns null
+     *
+     * @returns User if there acceptedRequest else returns null
+     */
     public User getBorrower() {
-        if(acceptedRequest != null) {
+        if (acceptedRequest != null) {
             return acceptedRequest.getRequestor();
         }
         return null;
@@ -76,29 +170,38 @@ public class Book implements Serializable {
 
     /**
      * should fail if acceptedRequest is not null.
+     *
      * @param request A request that has been accepted by owner
      */
     public void setAcceptedRequest(Request request) {
-        if(acceptedRequest == null) acceptedRequest = request;
+        if (acceptedRequest == null)
+            acceptedRequest = request;
+        status = Status.Accepted;
     }
 
+    /**
+     * Getter for acceptedRequest
+     * @return Request
+     */
     public Request getAcceptedRequest() {
         return acceptedRequest;
     }
 
+    /**
+     * getter for the photograph
+     *
+     * @return ImageView
+     */
     public ImageView getPhotograph() {
         return photograph;
     }
 
+    /**
+     * setter for photograph
+     *
+     * @param photograph ImageView
+     */
     public void setPhotograph(ImageView photograph) {
         this.photograph = photograph;
-    }
-
-    /*
-     * Uses the getters for each Request to fill in the views of a viewRequest layout
-     * (Probably going to be some form of a custom adapter layout)
-     */
-    public void viewRequests() {
-
     }
 }
--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/MyBooksActivity.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/MyBooksActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -23,11 +27,15 @@ import java.util.Map;
 public class MyBooksActivity extends AppCompatActivity {
 
     private Button addNew;
+    private Spinner mySpinner;
+    private String filter;
 
     private ArrayList<Book> myBooks = new ArrayList<Book>();
     BookListAdapter adapter;
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
+
+
 
     private FirebaseFirestore firestore;
 
@@ -35,48 +43,43 @@ public class MyBooksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_books);
-
+        mySpinner = (Spinner) findViewById(R.id.spinner);
         addNew = (Button) findViewById(R.id.btnAddNew);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinnner_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(adapter);
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            // When an item is selected in the spinner the results in the RecyclerView should change
+            // to show that
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    //
+                    case 0: filter = "All";
+                            break;
+                    // Case 1 is available
+                    case 1: filter = "Available";
+                            break;
+                    // case 2 is Requested
+                    case 2: filter = "Requested";
+                            break;
+                    // Case 3 is Accepted
+                    case 3: filter = "Accepted";
+                            break;
+                    // Case 4 is borrowed
+                    case 4: filter = "Borrowed";
+                            break;
 
-        //Testing purposes only, following two lines need to be removed
-        User u = new User("John", "n", 123);
-        /*Book book = new Book(u.getUserName(), "Author", "Title", 1234567890);
-        myBooks.add(book);
-        Book b = new Book(u.getUserName(), "Me", "Fuck 301", 1234567899);
-        myBooks.add(b);*/
-        firestore = FirebaseFirestore.getInstance();
+                }
+                query();
+            }
 
-        firestore.collection("Books").whereEqualTo("owner", u.getUserName()).get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Map<String, Object> temp = document.getData();
-                                Book book = new Book(temp.get("owner").toString(), temp.get("author").toString(),
-                                        temp.get("title").toString(), Long.valueOf(temp.get("isbn").toString()));
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-                                //TODO: Set all book information
-                                /*book.setPhotograph(temp.get("photograph").toString());
-                                book.setAcceptedRequest(temp.get("acceptedRequest").toString());
-                                book.setAvailable(temp.get("available").toString());
-                                book.setBorrower(temp.get("borrower").toString());
-                                book.setRequests(temp.get("requests").toString());*/
-
-                                myBooks.add(book);
-                            }
-
-                            recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-                            recyclerView.setHasFixedSize(true);
-                            layoutManager = new LinearLayoutManager(MyBooksActivity.this);
-                            recyclerView.setLayoutManager(layoutManager);
-                            BookListAdapter adapter = new BookListAdapter(MyBooksActivity.this, myBooks);
-                            recyclerView.setAdapter(adapter);
-                            adapter.notifyDataSetChanged();
-                        }
-                    }
-                });
-
+            }
+        });
         addNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -85,4 +88,71 @@ public class MyBooksActivity extends AppCompatActivity {
             }
         });
     }
+
+//        //Testing purposes only, following two lines need to be removed
+//        User u = new User("John", "n", 123);
+//        /*Book book = new Book(u.getUserName(), "Author", "Title", 1234567890);
+//        myBooks.add(book);
+//        Book b = new Book(u.getUserName(), "Me", "Fuck 301", 1234567899);
+//        myBooks.add(b);*/
+        public void query() {
+            firestore = FirebaseFirestore.getInstance();
+            final User u = new User("John", "n", 123);
+            final ArrayList<Book> newBooks = new ArrayList<Book>();
+            firestore.collection("Books").whereEqualTo("owner", u.getUserName()).get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Map<String, Object> temp = document.getData();
+                                    Book book = new Book(temp.get("owner").toString(),
+                                            temp.get("author").toString(),
+                                            temp.get("title").toString(),
+                                            Long.valueOf(temp.get("isbn").toString()));
+
+                                // TODO: Need to figure out how to get the request back to set properly for looks
+                                    //TODO: Set all book information
+                                    //                                book.setPhotograph(temp.get("photograph").toString());
+                                    //                                book.setAcceptedRequest(temp.get("acceptedRequest").toString());
+                                    //                                book.setAvailable(temp.get("available").toString());
+                                    //                                book.setBorrower(temp.get("borrower").toString());
+                                    //                                book.setRequests(temp.get("requests").toString());
+
+                                    if (filter.equals("All")) {
+                                        newBooks.add(book);
+                                    } else if (filter.equals("Available")) {
+                                        boolean available = (boolean) temp.get("available");
+                                        if (available) {
+                                            newBooks.add(book);
+                                        }
+                                    } else if (filter.equals("Requested")) {
+                                        if (temp.get("requests") != null) {
+                                            newBooks.add(book);
+                                        }
+                                    } else if (filter.equals("Accepted")) {
+                                        if (temp.get("acceptedRequest") != null) {
+                                            newBooks.add(book);
+                                    } else if (filter.equals("Borrowed")) {
+                                        if (temp.get("borrower") != null) {
+                                            newBooks.add(book);
+                                            }
+                                        }
+                                    }
+                                }
+
+                                recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+                                recyclerView.setHasFixedSize(true);
+                                layoutManager = new LinearLayoutManager(MyBooksActivity.this);
+                                recyclerView.setLayoutManager(layoutManager);
+                                BookListAdapter adapter = new BookListAdapter(MyBooksActivity.this,
+                                        newBooks);
+                                recyclerView.setAdapter(adapter);
+                                adapter.notifyDataSetChanged();
+
+                            }
+                        }
+                    });
+        }
+
 }
