c9672548ca1aaf5318d256e3a0556bd2c0226d43
Cohen Plontke <plontke@ualberta.ca>
2019-03-11 14:22:00 -0600

    Added Tests

---
+++ code/app/src/androidTest/java/ca/ualberta/cs/phebert/litx/MyBooksTest.java
@@ -0,0 +1,7 @@
+package ca.ualberta.cs.phebert.litx;
+
+public class MyBooksTest {
+    // TODO: Test to make sure that the book passed to ViewBook is the proper book
+    // TODO: Test to make sure the Edit does it properly and the delete
+    // TODO: TEst the add Book
+}
---
+++ code/app/src/androidTest/java/ca/ualberta/cs/phebert/litx/ScannerTest.java
@@ -0,0 +1,7 @@
+package ca.ualberta.cs.phebert.litx;
+
+public class ScannerTest {
+    // TODO: Make sure the title and Title and ISBN show up in the addBookActivity
+
+}
+
