cc78e31641fa52cc24c79115639d85a545ce7fea
Cohen Plontke <plontke@ualberta.ca>
2019-03-11 11:55:20 -0600

    Added status to Book and javadocced Book, added filtering to MyBooksActivity

--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/Book.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/Book.java
@@ -2,63 +2,167 @@ package ca.ualberta.cs.phebert.litx;
 
 import android.widget.ImageView;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Book {
+public class Book implements Serializable {
     private String author;
     private String title;
     private long isbn;
+    private Status status;
 
-    private User owner;
+
+    enum Status {
+        Available,
+        Borrowed,
+        Accepted,
+        Requested
+    }
+    // For testing the adapter
+    //private User borrower;
+
+    private String owner;
 
     private ArrayList<Request> requests;
     private Request acceptedRequest;
 
     private ImageView photograph;
 
-    public Book(User owner, String author, String title, long isbn) {
+    public Book(String owner, String author, String title, long isbn) {
         this.owner = owner;
         this.author = author;
         this.title = title;
         this.isbn = isbn;
     }
 
-    public void setOwner(User setowner) {
+    public Book() {
+    } // For firestore
+
+    /**
+     * sets Owner of the book
+     *
+     * @param owner string of the owner of the book
+     */
+    public void setOwner(String owner) {
         this.owner = owner;
     }
 
-    public User getOwner() {
+    /**
+     * getter for status
+     *
+     * @return String
+     */
+    public String getStatus() {
+        return this.status.toString();
+    }
+
+    /**
+     * Sets this.status to status
+     *
+     * @param status String one of accepted, available, borrowed, requested
+     */
+    public void setStatus(String status) {
+        if (status.equals("accepted")) {
+            this.status = Status.Accepted;
+        } else if (status.equals("available")) {
+            this.status = Status.Available;
+        } else if (status.equals("borrowed")) {
+            this.status = Status.Borrowed;
+        } else {
+            this.status = Status.Requested;
+        }
+
+    }
+
+    /**
+     * Getter for owner
+     *
+     * @return String
+     */
+    public String getOwner() {
         return owner;
     }
 
+    /**
+     * Returns if the book is available
+     *
+     * @return Boolean
+     */
     public Boolean isAvailable() {
-        return acceptedRequest == null;
+        return this.status == Status.Available;
     }
 
+    /**
+     * getter for author
+     *
+     * @return String
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * setter for Author
+     *
+     * @param author String
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /**
+     * getter for title
+     *
+     * @return String
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Setter for Title
+     *
+     * @param title String
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Getter for ISBN
+     *
+     * @return long type
+     */
     public long getIsbn() {
         return isbn;
     }
 
+    /**
+     * setter for ISBN
+     *
+     * @param isbn long type
+     */
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
 
+    /**
+     * getter for requests
+     *
+     * @returns ArrayList<Request>
+     */
     public ArrayList<Request> getRequests() {
         return requests;
     }
 
 
-    public void setBorrower(User user) {
-
-    }
-
+    /**
+     * Getter for borrower, if no acceptedRequest then returns null
+     *
+     * @returns User if there acceptedRequest else returns null
+     */
     public User getBorrower() {
-        if(acceptedRequest != null) {
+        if (acceptedRequest != null) {
             return acceptedRequest.getRequestor();
         }
         return null;
@@ -66,29 +170,38 @@ public class Book {
 
     /**
      * should fail if acceptedRequest is not null.
+     *
      * @param request A request that has been accepted by owner
      */
     public void setAcceptedRequest(Request request) {
-        if(acceptedRequest == null) acceptedRequest = request;
+        if (acceptedRequest == null)
+            acceptedRequest = request;
+        status = Status.Accepted;
     }
 
+    /**
+     * Getter for acceptedRequest
+     * @return Request
+     */
     public Request getAcceptedRequest() {
         return acceptedRequest;
     }
 
+    /**
+     * getter for the photograph
+     *
+     * @return ImageView
+     */
     public ImageView getPhotograph() {
         return photograph;
     }
 
+    /**
+     * setter for photograph
+     *
+     * @param photograph ImageView
+     */
     public void setPhotograph(ImageView photograph) {
         this.photograph = photograph;
-    }
-
-    /*
-     * Uses the getters for each Request to fill in the views of a viewRequest layout
-     * (Probably going to be some form of a custom adapter layout)
-     */
-    public void viewRequests() {
-
     }
 }
