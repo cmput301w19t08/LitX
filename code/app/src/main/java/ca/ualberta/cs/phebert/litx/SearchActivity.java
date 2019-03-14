b5de7a83d8a9300ceb6dbd6d7efbeb2b54846d21
thryniw2 <thryniw.2016@gmail.com>
2019-03-14 13:44:40 -0600

    adapter for book searching

--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/SearchActivity.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/SearchActivity.java
@@ -2,6 +2,8 @@ package ca.ualberta.cs.phebert.litx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
 
@@ -18,10 +20,18 @@ public class SearchActivity extends AppCompatActivity  {
 
     private ArrayList<Book> bookresults;
     String keywords;
+    BookListAdapter adapter;
+    RecyclerView recycler;
+    RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        recycler = (RecyclerView) findViewById(R.id.search_results);
+        recycler.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(SearchActivity.this);
+        recycler.setLayoutManager(layoutManager);
     }
 
     /**
@@ -59,8 +69,23 @@ public class SearchActivity extends AppCompatActivity  {
         }
     }*/
 
+    protected void updateRecycler () {
+        adapter = new BookListAdapter(SearchActivity.this, bookresults);
+        recycler.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+
     /*
      * Takes Search string and handles results
+     */
+
+    /**
+     * Actions on the press of the Search Button
+     * Get all the books on database
+     * Get filtered list
+     * Update the view
+     * @param v
      */
     public void searchPress (View v) {
         keywords = ((EditText)findViewById(R.id.input_search)).getText().toString();
@@ -83,6 +108,7 @@ public class SearchActivity extends AppCompatActivity  {
                             }
                         }
                         bookresults = findBook(bookresults, keywords.split(" "));
+                        updateRecycler();
                     }
                 });
     }
