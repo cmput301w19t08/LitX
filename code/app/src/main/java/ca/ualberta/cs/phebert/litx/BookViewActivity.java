767d46cbd7018f83565c90065739ace8966d211b
Scott-Dupasquier <sdupasqu@ualberta.ca>
2019-03-20 17:47:22 -0600

    Started working on photos

--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/BookViewActivity.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/BookViewActivity.java
@@ -1,16 +1,13 @@
 package ca.ualberta.cs.phebert.litx;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
@@ -41,16 +38,15 @@ public class BookViewActivity extends AppCompatActivity {
         Intent intent = getIntent();
         final Book book = (Book) intent.getExtras().getSerializable("Book");
 
-        // Set descriptiption of book in the textview
+        // Set description of book in the textview
         ImageView image = (ImageView) findViewById(R.id.bookImage);
         TextView textView = (TextView) findViewById(R.id.descriptionIDView);
         String description = "Title: " + book.getTitle() + "\n" + "Author: " + book.getAuthor()
                 + "\n" + "ISBN: " + String.valueOf(book.getIsbn());
         textView.setText(description);
-        image = book.getPhotograph();
 
         // Find buttons in the layout
-        delete = (Button) findViewById(R.id.deleteButtonID);
+        delete = (Button) findViewById(R.id.deleteButton);
         edit = (Button) findViewById(R.id.editButtonID);
 
         firestore = FirebaseFirestore.getInstance();
@@ -74,6 +70,16 @@ public class BookViewActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //Pass Book object into AddBookActivity and start the activity
                 Intent intent = new Intent(BookViewActivity.this, AddBookActivity.class);
+                intent.putExtra("Book", book);
+                startActivity(intent);
+            }
+        });
+
+        // Set an onClickListener for the photo that launches the view photo activity
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BookViewActivity.this, ViewPhotoActivity.class);
                 intent.putExtra("Book", book);
                 startActivity(intent);
             }
