667e6a95c2d004894a712d8512bde0f8e4a80005
phebert5009 <cpierrot273@gmail.com>
2019-03-21 15:20:28 -0600

    Fixed templated code.

--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/Request.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/Request.java
@@ -29,7 +29,7 @@ public class Request {
 
             @Override
             public Status refuse(Book toModify) {
-                return super.refuse();
+                return Refused;
             }
         },
         Accepted("accepted") {
