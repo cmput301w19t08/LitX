fe6557949801929d372ed094d40ecd3aa065e38b
Scott-Dupasquier <sdupasqu@ualberta.ca>
2019-03-27 09:00:45 -0600

    Fixed image loading, added a progress tracker for uploading photos

--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/BookViewActivity.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/BookViewActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
@@ -119,6 +122,7 @@ public class BookViewActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(BookViewActivity.this, ViewPhotoActivity.class);
                 intent.putExtra("Book", book);
+                finish();
                 startActivity(intent);
             }
         });
@@ -128,14 +132,15 @@ public class BookViewActivity extends AppCompatActivity {
     private void load_image(Book book) {
         // Load the image into the imageview if it exists
         StorageReference storageReference = FirebaseStorage.getInstance().getReference();
-        try {
-            int iconId = this.getResources().getIdentifier("book_icon", "drawable", this.getPackageName());
-            StorageReference pathReference = storageReference.child(book.getOwnerUid() + "/" + Long.toString(book.getIsbn()));
-            //StorageReference pathReference = storageReference.child("sdupasqu-1.png");
-            GlideApp.with(this)
-                    .load(pathReference)
-                    .placeholder(iconId)
-                    .into(photo);
-        } catch (Exception e) {}
+        int iconId = this.getResources().getIdentifier("book_icon", "drawable", this.getPackageName());
+        StorageReference pathReference = storageReference.child(book.getOwnerUid() + "/" + Long.toString(book.getIsbn()));
+
+        pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String imageURL = uri.toString();
+                GlideApp.with(BookViewActivity.this).load(imageURL).placeholder(iconId).into(photo);
+            }
+        });
     }
 }
--- code/app/src/main/java/ca/ualberta/cs/phebert/litx/ViewPhotoActivity.java
+++ code/app/src/main/java/ca/ualberta/cs/phebert/litx/ViewPhotoActivity.java
@@ -3,18 +3,27 @@ package ca.ualberta.cs.phebert.litx;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 /**
  * Activity for viewing a photo when selected from the view book activity. This activity implements
@@ -29,6 +38,15 @@ public class ViewPhotoActivity extends AppCompatActivity {
     Uri uri;
 
     private ImageView ivPhoto;
+    private Button btn_add;
+    private Button btn_delete;
+    private Button btn_done;
+    private Button btn_cancel;
+    private ProgressBar progressBar;
+    private TextView tvProgress;
+
+    private int iconId;
+    private Boolean cancel;
 
     private StorageReference storageRef;
     private StorageReference pathReference;
@@ -37,18 +55,23 @@ public class ViewPhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_photo);
+        cancel = false;
 
-        Button btn_add = (Button) findViewById(R.id.addPhotoButton);
-        Button btn_delete = (Button) findViewById(R.id.deletePhotoButton);
-        Button btn_done = (Button) findViewById(R.id.donePhotoButton);
+        btn_add = (Button) findViewById(R.id.addPhotoButton);
+        btn_delete = (Button) findViewById(R.id.deletePhotoButton);
+        btn_done = (Button) findViewById(R.id.donePhotoButton);
+        btn_cancel = (Button) findViewById(R.id.cancelUploadButton);
         ivPhoto = (ImageView) findViewById(R.id.photoImageView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        tvProgress = (TextView) findViewById(R.id.progressTextView);
         storageRef = FirebaseStorage.getInstance().getReference();
 
         Intent intent = getIntent();
         final Book book = (Book) intent.getExtras().getSerializable("Book");
 
         pathReference = storageRef.child(book.getOwnerUid() + "/" + Long.toString(book.getIsbn()));
-        load_image(book);
+        iconId = this.getResources().getIdentifier("book_icon", "drawable", this.getPackageName());
+        load_image();
 
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +87,7 @@ public class ViewPhotoActivity extends AppCompatActivity {
                 // Delete the file
                 try {
                     pathReference.delete();
+                    GlideApp.with(ViewPhotoActivity.this).load(iconId).into(ivPhoto);
                     Log.d("Success", "File has been deleted");
                 } catch(Exception e) {
                     Log.d("Error", "Error occured, file could not be deleted!");
@@ -74,13 +98,17 @@ public class ViewPhotoActivity extends AppCompatActivity {
         btn_done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (uri != null) {
-                    pathReference.putFile(uri);
-                }
-
                 Intent view_book = new Intent(ViewPhotoActivity.this, BookViewActivity.class);
                 view_book.putExtra("Book", book);
+                finish();
                 startActivity(view_book);
+            }
+        });
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancel = true;
             }
         });
     }
@@ -89,16 +117,59 @@ public class ViewPhotoActivity extends AppCompatActivity {
         super.onActivityResult(request, result, intent);
         if (result == RESULT_OK && request == image) {
             uri = intent.getData();
+            if (uri != null) {
+                UploadTask upload = pathReference.putFile(uri);
+                btn_add.setVisibility(View.GONE);
+                btn_done.setVisibility(View.GONE);
+                btn_delete.setVisibility(View.GONE);
+                btn_cancel.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
+                tvProgress.setVisibility(View.VISIBLE);
+
+                upload.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        Log.d("Upload", "Completed");
+                        show_buttons();
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                        if (!cancel) {
+                            long progress = (100 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                            progressBar.setProgress((int) progress);
+                            tvProgress.setText("Uploading... " + Long.toString(progress) + "% done");
+                        } else {
+                            upload.cancel();
+                            show_buttons();
+                            load_image();
+                            cancel = false;
+                        }
+                    }
+                });
+            }
 
             Glide.with(this).load(uri).into(ivPhoto);
         }
     }
 
-    private void load_image(Book book) {
+    private void load_image() {
         // Load the image into the imageview if it exists
-        try {
-            int iconId = this.getResources().getIdentifier("book_icon", "drawable", this.getPackageName());
-            GlideApp.with(this).load(pathReference).placeholder(iconId).into(ivPhoto);
-        } catch (Exception e) {}
+        pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String imageURL = uri.toString();
+                GlideApp.with(ViewPhotoActivity.this).load(imageURL).placeholder(iconId).into(ivPhoto);
+            }
+        });
+    }
+
+    private void show_buttons() {
+        btn_add.setVisibility(View.VISIBLE);
+        btn_delete.setVisibility(View.VISIBLE);
+        btn_done.setVisibility(View.VISIBLE);
+        btn_cancel.setVisibility(View.GONE);
+        progressBar.setVisibility(View.GONE);
+        tvProgress.setVisibility(View.GONE);
     }
 }
